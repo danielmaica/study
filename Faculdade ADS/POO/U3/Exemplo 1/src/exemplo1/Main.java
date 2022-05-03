6b0c3493c3660e9b88c3ac3977beb93b7fac737c
danielmaica <danielmaica99@gmail.com>
2022-05-02 23:12:47 -0300

    up

--- Faculdade ADS/POO/U3/Exemplo 1/src/exemplo1/Main.java
+++ Faculdade ADS/POO/U3/Exemplo 1/src/exemplo1/Main.java
@@ -1,8 +1,10 @@
 package exemplo1;
 
 public class Main {
-
     public static void main(String[] args) {
-	// write your code here
+        int soma = 0;
+	    for (int i = 0; i <= 80; i++)
+            soma = i + i;
+        System.out.println(soma);
     }
 }
