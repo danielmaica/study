186b82ef0fad01264a227f1bdbd5e00fa0e55f80
danielmaica <danielmaica99@gmail.com>
2022-08-24 14:35:04 -0300

    feat: instalação e configuração eslint

--- Diversos/youtube/react-project/curso-react/src/App.tsx
+++ Diversos/youtube/react-project/curso-react/src/App.tsx
@@ -1,9 +1,9 @@
 // dependências - módulos
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from 'react-router-dom';
 
 // componentes
-import { AppRoutes } from "./routes/AppRoutes";
-import { AppThemeProvider } from "./shared/contexts";
+import { AppRoutes } from './routes/AppRoutes';
+import { AppThemeProvider } from './shared/contexts';
 
 export const App = () => {
   return (
--- Diversos/youtube/react-project/curso-react/src/routes/AppRoutes.tsx
+++ Diversos/youtube/react-project/curso-react/src/routes/AppRoutes.tsx
@@ -3,14 +3,20 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import { useAppThemeContext } from '../shared/contexts';
 
 export const AppRoutes = () => {
-  
   const { toggleTheme } = useAppThemeContext();
-  
+
   return (
     <Routes>
-      <Route path='/pagina-inicial' element={<Button variant='contained' color='primary' onClick={toggleTheme}>Toogle Theme</Button>} />
+      <Route
+        path="/pagina-inicial"
+        element={
+          <Button variant="contained" color="primary" onClick={toggleTheme}>
+            Toogle Theme
+          </Button>
+        }
+      />
 
-      <Route path='*' element={<Navigate to='/pagina-inicial' />} />
+      <Route path="*" element={<Navigate to="/pagina-inicial" />} />
     </Routes>
-  )
-}
+  );
+};
