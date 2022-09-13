b727b1870ef500571344fc7f217d234325da6693
danielmaica <danielmaica99@gmail.com>
2022-09-13 10:14:47 -0300

    feat: html componente login pronto

---
+++ Diversos/Udemy/Angular/ChatBotFluidFrontend/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <app-login></app-login>
+    <router-outlet></router-outlet>
+  `
+})
+
+export class AppComponent implements OnInit{
+
+  constructor() { }
+
+  ngOnInit(): void { }
+
+}
---
+++ Diversos/Udemy/Angular/ChatBotFluidFrontend/src/app/login/login.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+
+  public title: string = "Bem-vindo, faça seu login!"
+  
+  constructor() { }
+
+  ngOnInit(): void { }
+
+}
